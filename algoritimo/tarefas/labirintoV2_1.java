5e545eced3907cc825dc51a8b061440ce9e20947
guisiq <lopesgui01@hotmail.com>
2020-06-08 22:54:35 -0300

    melhora no algoritimo gerador das paredes

--- algoritimo/tarefas/labirintoV2_1.java
+++ algoritimo/tarefas/labirintoV2_1.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
-public class labirintoV2_0 {
-    public static void main(String[] args) {
+public class labirintoV2_1 {
+    public static void main(final String[] args) {
         boolean expandiu = true;
         boolean camilho = false;
 
-        Random aleatorio = new Random();
+        final Random aleatorio = new Random();
         int labirintosSemSaida = 0;        
-        int[][] labirinto = new int[20][20];
+        final int[][] labirinto = new int[20][20];
         //0 espaco em branco
-        //1 paredes
+        //1 e 5 paredes
         //2 para começo e fim  
         //3 e 4 reservada para testar caminho
         //lembrete pra concerta os nomes da variaveis linhas e coluna que estao trocadas 
@@ -23,20 +23,61 @@ public class labirintoV2_0 {
                 for (int linha = 0; linha < labirinto[coluna].length; linha++) {
                     if (coluna == 0 || coluna == labirinto.length - 1 || linha == 0
                             || linha == labirinto[0].length - 1) {
-                        labirinto[coluna][linha] = 1;
+                        labirinto[coluna][linha] = 5;
                     } else {
                         labirinto[coluna][linha] = 0;
                     }
                 }
             }
             // gerando paredes aleatorias 
-            for (int i = 0; i < 150; i++) {
+            /*obs:tirando as paredes ha 324 elementos para o melhor funcionamento defina i para menos 
+            que a metade disso(162)numeros mais aultos gerram maior probabilidade de lobirintos sem saidas 
+            para comparacao com i = 150 gerou 16765 labirintos sem saida
+            */
+            for (int i = 0; i < 140; i++) {
                 int x;
                 int y;
+                int aredor = 0;
                 x = aleatorio.nextInt(20);
                 y = aleatorio.nextInt(20);
                 if (labirinto[x][y] == 0) {
-                    labirinto[x][y] = 1;
+                    // cauculando os aredores da posiçao selecionada pelo sorteio e armazenando em aredor
+                    // se aredor for maior que 4 sera sorteado outra posisao para as paredes ficarem mais espalhadas  
+                    if (labirinto[x + 1][y + 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x + 1][y - 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x + 1][y] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x][y + 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x][y - 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x - 1][y - 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x - 1][y + 1] == 1) {
+                        aredor++;
+                    }
+                    if (labirinto[x - 1][y] == 1) {
+                        aredor++;
+                    }
+                    //testando se o ponto escolhido  esta colado em auguma parede 
+                    if (labirinto[x + 1][y-1] == 5 || labirinto[x + 1][y + 1] == 5 || labirinto[x - 1][y-1] ==5|| labirinto[x - 1][y+1] ==5) {
+                        aredor = aredor + 1 ;
+                    }
+                    if (aredor <= 3) {
+                        labirinto[x][y] = 1;
+                        aredor = 0;
+                    } else {
+                        i--;
+                        aredor = 0;
+                    }
                 } else {
                     i--;
                 }
@@ -133,7 +174,7 @@ public class labirintoV2_0 {
                 if (labirinto[coluna][linha] == 0 ||labirinto[coluna][linha] == 3 ||labirinto[coluna][linha] == 4 ) {
                 System.out.print("  ");    
                 }else{
-                System.out.print(labirinto[coluna][linha]+" ");
+                System.out.print("# ");
                 }
             }
             System.out.println();
